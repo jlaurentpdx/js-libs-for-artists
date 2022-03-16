f44c35219a2a9cebfbfb2edc7aef6848ea55e1ac
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-03-15 17:26:44 -0700

    added findAll method to model, tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,4 +25,27 @@ describe('artist-libs-api routes', () => {
 
     expect(response.body).toEqual({ id: expect.any(String), ...expected });
   });
+
+  it('should read a list of libraries from the API', async () => {
+    const expected = [
+      {
+        id: '1',
+        name: 'tone.js',
+        url: 'https://tonejs.github.io/',
+        description:
+          'Tone.js is a Web Audio framework for creating interactive music in the browser.',
+      },
+      {
+        id: '2',
+        name: 'p5.js',
+        url: 'https://p5js.org/',
+        description:
+          'p5.js is a JavaScript library for creative coding, with a focus on making coding accessible and inclusive for artists, designers, educators, beginners, and anyone else!',
+      },
+    ];
+
+    const response = await request(app).get('/api/v1/libraries');
+
+    expect(response.body).toEqual(expected);
+  });
 });
